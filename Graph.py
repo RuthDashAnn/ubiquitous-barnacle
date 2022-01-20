6202f1f0ef836527158e049a2e850e3364ac56f3
RuthDashAnn <77833413+RuthDashAnn@users.noreply.github.com>
2022-01-20 11:51:33 +0000

    Add files via upload

---
+++ Graph.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from plotly.subplots import make_subplots
+from ipywidgets import interact, interactive, fixed, interact_manual
+import ipywidgets as widgets
+import plotly.express as px
+
+
+
+#Reading the file
+
+df = pd.read_csv("C:\\Users\Ruth's PC\OneDrive\Documents\Essex\Second Year\CE201-5-FY Team Project\Individual Project\ExamResults1.csv")
+
+
+fig = px.scatter(df, x="Average", y="CSE101")
+
+
+
+fig.show()
